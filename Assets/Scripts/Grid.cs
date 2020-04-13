301257cf8fa932f15f3ad557c0013b7fed9973ab
Hiba Jamal <hj04033@st.habib.edu.pk>
2020-04-13 23:08:46 +0500

    algo working but positioning bug

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -10,29 +10,43 @@ public class GridBlock
     public int x;
     public int y;
     public GameObject obj;
+    public float reward;
     public float value;
+    public bool invalMove;
+    public GameObject direction;
 
     public GridBlock()
     {
         x = y = 0;
-        obj = null;
-        value = -1;
+        obj = direction = null;
+        value = reward = -1;
+        invalMove = false;
     }
 
-    public GridBlock(int x, int y, GameObject obj, float value)
+    public GridBlock(int x, int y, GameObject obj, float value, GameObject direction, float reward, bool invalMove)
     {
         this.x = x;
         this.y = y;
         this.obj = obj;
         this.value = value;
+        this.direction = direction;
+        this.reward = reward;
+        this.invalMove = invalMove;
     }
 }
 
 public class Grid : MonoBehaviour
 {
+    // Grid Blocks
     public GameObject redBlock;
     public GameObject greenBlock;
     public GameObject plainBlock;
+
+    // Block Arrows
+    public GameObject left;
+    public GameObject up;
+    public GameObject down;
+    public GameObject right;
 
     public List<List<GridBlock>> grid = new List<List<GridBlock>>();
 
@@ -40,10 +54,11 @@ public class Grid : MonoBehaviour
 
     //private bool rewardSet = false;
     private float tileSize = .9f;
-
+    private float t;
     // Start is called before the first frame update
     void Start()
     {
+        t = 0;
         Vector3 worldStart = GetComponent<RectTransform>().transform.position;
         //for (int i=0; i<10; i++)
         int i = -1;
@@ -59,22 +74,29 @@ public class Grid : MonoBehaviour
                 if (el == '2')
                 {
                     block.obj = Instantiate(greenBlock, this.transform);
-                    block.value = 100;
+                    block.value = 0;
+                    block.reward = 100;
+                    block.invalMove = true;
                 }
                 else if (el == '1')
                 {
                     block.obj = Instantiate(redBlock, this.transform);
-                    block.value = -100;
+                    block.value = -0;
+                    block.reward = -100;
+                    block.invalMove = true;
                 }
                 else
                 {
                     block.obj = Instantiate(plainBlock, this.transform);
                     block.value = -1;
+                    block.reward = 0;
+                    block.invalMove = false;
                 }
                 // placement
                 block.obj.GetComponent<RectTransform>().transform.position = new Vector3(worldStart.x + tileSize / 2 + (tileSize * j),
                                                               worldStart.y - tileSize / 2 - (tileSize * i),
                                                               0);
+                Debug.Log(block.obj.GetComponent<RectTransform>().transform.position);
                 block.x = i;
                 block.y = j;
                 gridline.Add(block);
@@ -82,12 +104,85 @@ public class Grid : MonoBehaviour
             }
             grid.Add(gridline);
         }
-        mapSet = true;
+        mapSet = true;        
+    }
+
+    void ValueIteration(float theta, float discount)
+    {
+        bool convergence = false;
+        // one iteration through entire grid
+        for (int i=0; i<10; i++) {
+            for (int j=0; j<10; j++) {
+                float oldVal = grid[i][j].value;
+                float value;
+                bool placed = false;
+                // for up
+                if (i > 0 && !grid[i][j].invalMove) {
+                    value = grid[i - 1][j].reward + (discount * grid[i - 1][j].value);
+                    if (value > grid[i][j].value) {
+                        grid[i][j].value = value;
+                        if (grid[i][j].direction != null)
+                            Destroy(grid[i][j].direction);
+                        grid[i][j].direction = Instantiate(up, grid[i][j].obj.GetComponent<RectTransform>().transform);
+                        placed = true;
+                    }
+                }
+                // for right
+                if (j < 9 && !grid[i][j].invalMove) {
+                    value = grid[i][j + 1].reward + (discount * grid[i][j + 1].value);
+                    if (value > grid[i][j].value) {
+                        grid[i][j].value = value;
+                        if (grid[i][j].direction != null)
+                            Destroy(grid[i][j].direction);
+                        grid[i][j].direction = Instantiate(right, grid[i][j].obj.GetComponent<RectTransform>().transform);
+                        placed = true;
+                    }
+                }
+                // for left 
+                if (j > 0 && !grid[i][j].invalMove) {
+                    value = grid[i][j - 1].reward + (discount * grid[i][j - 1].value);
+                    if (value > grid[i][j].value) {
+                        grid[i][j].value = value;
+                        if (grid[i][j].direction != null)
+                            Destroy(grid[i][j].direction);
+                        grid[i][j].direction = Instantiate(left, grid[i][j].obj.GetComponent<RectTransform>().transform);
+                        placed = true;
+                    }
+                }
+                // for down
+                if (i < 9 && !grid[i][j].invalMove) {
+                    value = grid[i + 1][j].reward + (discount * grid[i+1][j].value);
+                    if (value > grid[i][j].value) {
+                        grid[i][j].value = value;
+                        if (grid[i][j].direction != null)
+                            Destroy(grid[i][j].direction);
+                        grid[i][j].direction = Instantiate(down, grid[i][j].obj.GetComponent<RectTransform>().transform);
+                        placed = true;
+                    }
+                }
+                if (placed) {
+                    grid[i][j].direction.GetComponent<RectTransform>().transform.position = new Vector3(grid[i][j].obj.GetComponent<RectTransform>().transform.position.x,
+                                                                                                grid[i][j].obj.GetComponent<RectTransform>().transform.position.y,
+                                                                                                -1);
+                    Debug.Log("placed");
+                }
+                if (Mathf.Abs(oldVal - grid[i][j].value) <= theta) { 
+                    convergence = true;
+                    //break;
+                }
+            }
+            /*if (convergence)
+                break;*/
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (t % 10 == 0) { 
+            ValueIteration(0.01f, 0.9f);
+            Debug.Log("one run through value iter done");
+        }
+        t++;
     }
 }
